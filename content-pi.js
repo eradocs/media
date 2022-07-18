bf2e588314cb1108eb90771fdd54013fa00449ee
Era Docs <104847965+eradocs@users.noreply.github.com>
2022-07-18 21:50:53 +0530

    Create content-pi.js

---
+++ content-pi.js
@@ -0,0 +1,228 @@
+// Copyright 2022 Era Corner. All Rights Reserved.
+
+
+// Header Ads
+! function() {
+	function f() {
+		var a = document.getElementById("header-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+// Home Ads
+! function() {
+	function f() {
+		var a = document.getElementById("home-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Footer Ads
+! function() {
+	function f() {
+		var a = document.getElementById("footer-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Header Ads
+! function() {
+	function f() {
+		var a = document.getElementById("header-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Post Before Ads
+! function() {
+	function f() {
+		var a = document.getElementById("before-ad");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Post After Ads
+! function() {
+	function f() {
+		var a = document.getElementById("after-ad");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Post Footer Ads
+! function() {
+	function f() {
+		var a = document.getElementById("post-footer-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Sticky Sidebar Ads
+! function() {
+	function f() {
+		var a = document.getElementById("sidebar-ads");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// In Article Ads 1
+! function() {
+	function f() {
+		var a = document.getElementById("inarticle-1");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// In Article Ads 2
+! function() {
+	function f() {
+		var a = document.getElementById("inarticle-2");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+
+// In Article Ads 3
+! function() {
+	function f() {
+		var a = document.getElementById("inarticle-3");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
+
+
+
+// Sticky Bottom Ads (1200×150)
+! function() {
+	function f() {
+		var a = document.getElementById("pi-bar");
+		a.innerHTML = ``
+	}
+	var b = document.createElement("script");
+	b.async = !0;
+	b.src = "https://tpc.googlesyndication.com/pagead/js/r20220706/r20110914/client/load_preloaded_resource_fy2021.js";
+	b.onerror = function() {
+		f();
+		window.scriptblock = !0
+	};
+	var e = document.getElementsByTagName("script")[0];
+	e.parentNode.insertBefore(b, e)
+}();
